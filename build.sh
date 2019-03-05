2a5a40700872dfb2205e630e92ef68db89baf12b
Ben Wiklund <ben@daisyowl.com>
2019-03-04 16:50:56 -0800

    ng

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-rm -rf build
-mkdir build
-./node_modules/.bin/coffee --output build/ -c source/*.coffee && \
-  ./node_modules/.bin/stylus source/*.styl -o build/ && \
-  ./node_modules/.bin/pug source/*.pug -o build/ && \
-  cp source/angular.min.js build/
