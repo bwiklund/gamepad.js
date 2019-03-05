5bf33effff005c5f2a34bbebe281d9fa0261f617
Ben Wiklund <ben@daisyowl.com>
2019-03-04 17:23:36 -0800

    converted template and styling to ng2

--- src/app/gamepad-list/gamepad-list.component.ts
+++ src/app/gamepad-list/gamepad-list.component.ts
@@ -14,4 +14,27 @@ export class GamepadListComponent implements OnInit {
     this.gamepads = navigator.getGamepads();
   }
 
+  axisStyle(n) {
+    opacity: Math.abs(n) + 0.3
+  }
+
+  buttonStyle(id) {
+    var val = this.buttonValue(id);
+    return {
+      opacity: Math.abs(val) + 0.3,
+      border: this.buttonPressed(id) ? '1px solid #888' : '1px solid transparent'
+    }
+  }
+
+  buttonValue(b) {
+    return (typeof (b) == 'number') ? b : b.value;
+  }
+
+  buttonPressed(b) {
+    return (typeof (b) == 'number') ? b > 0.1 : b.pressed
+  }
+
+  mappingString(mapping: GamepadMappingType) {
+    return mapping || '[none]';
+  }
 }
